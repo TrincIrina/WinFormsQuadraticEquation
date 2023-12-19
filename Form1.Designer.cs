566831d25251ea46660ad100fd60d20822657ab7
User <User@HOME>
2023-12-19 23:51:16 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WinFormsQuadraticEquation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.KTextBox1 = new System.Windows.Forms.TextBox();
+            this.KTextBox2 = new System.Windows.Forms.TextBox();
+            this.KTextBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.AnswerTextBox = new System.Windows.Forms.TextBox();
+            this.SolutionButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(413, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите коэфициенты уравнения:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(113, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "x^2 +";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(283, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "x +";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(429, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "= 0";
+            // 
+            // KTextBox1
+            // 
+            this.KTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.KTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.KTextBox1.ForeColor = System.Drawing.Color.White;
+            this.KTextBox1.Location = new System.Drawing.Point(17, 66);
+            this.KTextBox1.Name = "KTextBox1";
+            this.KTextBox1.Size = new System.Drawing.Size(90, 33);
+            this.KTextBox1.TabIndex = 4;
+            this.KTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KTextBox2
+            // 
+            this.KTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.KTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.KTextBox2.ForeColor = System.Drawing.Color.White;
+            this.KTextBox2.Location = new System.Drawing.Point(187, 66);
+            this.KTextBox2.Name = "KTextBox2";
+            this.KTextBox2.Size = new System.Drawing.Size(90, 33);
+            this.KTextBox2.TabIndex = 5;
+            this.KTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KTextBox3
+            // 
+            this.KTextBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.KTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.KTextBox3.ForeColor = System.Drawing.Color.White;
+            this.KTextBox3.Location = new System.Drawing.Point(333, 65);
+            this.KTextBox3.Name = "KTextBox3";
+            this.KTextBox3.Size = new System.Drawing.Size(90, 33);
+            this.KTextBox3.TabIndex = 6;
+            this.KTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Ответ:";
+            // 
+            // AnswerTextBox
+            // 
+            this.AnswerTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.AnswerTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AnswerTextBox.ForeColor = System.Drawing.Color.White;
+            this.AnswerTextBox.Location = new System.Drawing.Point(107, 119);
+            this.AnswerTextBox.Name = "AnswerTextBox";
+            this.AnswerTextBox.Size = new System.Drawing.Size(386, 33);
+            this.AnswerTextBox.TabIndex = 8;
+            // 
+            // SolutionButton
+            // 
+            this.SolutionButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.SolutionButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SolutionButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.SolutionButton.Location = new System.Drawing.Point(12, 180);
+            this.SolutionButton.Name = "SolutionButton";
+            this.SolutionButton.Size = new System.Drawing.Size(150, 50);
+            this.SolutionButton.TabIndex = 9;
+            this.SolutionButton.Text = "Решить";
+            this.SolutionButton.UseVisualStyleBackColor = false;
+            this.SolutionButton.Click += new System.EventHandler(this.SolutionButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitButton.ForeColor = System.Drawing.Color.Red;
+            this.ExitButton.Location = new System.Drawing.Point(343, 180);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(150, 50);
+            this.ExitButton.TabIndex = 10;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClearButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClearButton.Location = new System.Drawing.Point(177, 180);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(150, 50);
+            this.ClearButton.TabIndex = 11;
+            this.ClearButton.Text = "Очистить";
+            this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(505, 251);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.SolutionButton);
+            this.Controls.Add(this.AnswerTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.KTextBox3);
+            this.Controls.Add(this.KTextBox2);
+            this.Controls.Add(this.KTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Решение квадратного уравнения";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox KTextBox1;
+        private System.Windows.Forms.TextBox KTextBox2;
+        private System.Windows.Forms.TextBox KTextBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox AnswerTextBox;
+        private System.Windows.Forms.Button SolutionButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinFormsQuadraticEquation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            // Кнопка закрытия окна
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Выход - использование диалогового окна
+            DialogResult result = MessageBox.Show(
+                "Вы действительно хотите выйти?",
+                "Выход",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void SolutionButton_Click(object sender, EventArgs e)
+        {            
+            using (StreamWriter sw = new StreamWriter("QuadraticEquation.txt", true))
+            {
+                sw.WriteLine(label1.Text);
+                // Считывание введённых коэфициентов
+                string userInput1 = KTextBox1.Text;
+                string userInput2 = KTextBox2.Text;
+                string userInput3 = KTextBox3.Text;                
+                sw.Write(userInput1);
+                sw.Write(label2.Text);
+                sw.Write(userInput2);
+                sw.Write(label3.Text);
+                sw.Write(userInput3);
+                sw.WriteLine(label4.Text);
+                // Коэфициенты уравнения
+                double k1, k2, k3;
+                // Исключение некорректного ввода чисел
+                if (double.TryParse(userInput1, out k1)
+                    && double.TryParse(userInput2, out k2)
+                    && double.TryParse(userInput3, out k3))
+                {
+                    k1 = Convert.ToDouble(userInput1);
+                    k2 = Convert.ToDouble(userInput2);
+                    k3 = Convert.ToDouble(userInput3);
+                    double D = k2 * k2 - 4 * k1 * k3;  // Нахождение Дискриминанта
+                    sw.WriteLine($"D = {D}");
+                    sw.Write(label5.Text);
+                    if (D > 0)                         // Д > 0 - уравнение имеет два корня
+                    {
+                        // Нахождение и вывод корней
+                        double x1 = (-k2 + Math.Sqrt(D)) / (2 * k1);
+                        double x2 = (-k2 - Math.Sqrt(D)) / (2 * k1);
+                        AnswerTextBox.Text = $"x1 = {x1}; x2 = {x2}";
+                        sw.WriteLine($"x1 = {x1}; x2 = {x2}");
+                    }
+                    if (D == 0)                       // Д = 0 - уравнение имеет один корень
+                    {
+                        // Нахождение и вывод корня 
+                        double x = -k2 / (2 * k1);
+                        AnswerTextBox.Text = $"x = {x}";
+                        sw.WriteLine($"x = {x}");
+                    }
+                    if (D < 0)                         // Д < 0 - уравнение не имеет корней
+                    {
+                        // Вывод ответа
+                        AnswerTextBox.Text = "Корней нет";
+                        sw.WriteLine("Корней нет");
+                    }                    
+                }
+                else
+                {
+                    // Вывод окна "Ошибка", очистка всех тектбоксов
+                    MessageBox.Show(
+                        "Некорректный ввод",
+                        "Ошибка",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    KTextBox1.Clear();
+                    KTextBox2.Clear();
+                    KTextBox3.Clear();
+                    AnswerTextBox.Clear();
+                }
+                sw.WriteLine();
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            // Кнопка очистки текстбоксов
+            KTextBox1.Clear();
+            KTextBox2.Clear();
+            KTextBox3.Clear();
+            AnswerTextBox.Clear();
+        }
+    }
+}
